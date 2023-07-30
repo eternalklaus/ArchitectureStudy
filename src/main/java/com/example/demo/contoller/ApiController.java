e1323b853cb697443aaa574a519cc1384a8233df
eternalklaus <eternalklaus@naver.com>
2023-07-30 10:12:21 +0900

    코드리뷰 반영: Swagger springfox -> springdoc으로 변경

--- src/main/java/com/example/demo/contoller/ApiController.java
+++ src/main/java/com/example/demo/contoller/ApiController.java
@@ -1,13 +1,11 @@
 package com.example.demo.contoller;
 
-import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = {"API 정보를 제공하는 Controller"})
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
