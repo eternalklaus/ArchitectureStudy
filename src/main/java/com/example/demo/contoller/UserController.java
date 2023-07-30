c15302079793da96923359d51c3a28253f758365
eternalklaus <eternalklaus@naver.com>
2023-07-30 23:08:36 +0900

    Spring Security 설정

--- src/main/java/com/example/demo/contoller/UserController.java
+++ src/main/java/com/example/demo/contoller/UserController.java
@@ -2,18 +2,18 @@ package com.example.demo.contoller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/v1")
 @RequiredArgsConstructor
-public class ApiController {
+@Controller
+@RequestMapping("/user")
+public class UserController {
 
-    @GetMapping("/todolist")
-    ResponseEntity<String> test(){
-        return ResponseEntity.ok("todolist");
+    @GetMapping("/login") // @PostMapping 방식의 메서드는 스프링 시큐리티가 대신 처리하므로 직접 구현할 필요가 없다.
+    public String login() {
+        return "login_form";
     }
 }
