15138f1a7db848dcc349bfde67ef49ca04b886ab
eternalklaus <eternalklaus@naver.com>
2023-09-09 23:22:37 +0900

    Radisson 캐싱 구현

--- src/main/java/com/example/demo/contoller/ApiController.java
+++ src/main/java/com/example/demo/contoller/ApiController.java
@@ -20,15 +20,14 @@ public class ApiController {
         return ResponseEntity.ok("todolist");
     }
 
-    @GetMapping("/viewcoupon")
-    ResponseEntity<String> viewCoupon() {
-        // TODO: 사용자 정보를 바탕으로 쿠폰을 가져온다
-        // TODO: 로컬의 래디스캐시에 쿠폰리스트가 저장되도록 최적화한다
-        return ResponseEntity.ok("당신의 쿠폰 리스트는: ");
+    @GetMapping("/showcoupon")
+    ResponseEntity<Coupon> viewCoupon() {
+        Coupon coupon = couponService.show(1L);
+        return ResponseEntity.ok(coupon);
     }
 
     @GetMapping("/usecoupon")
-    ResponseEntity<Coupon> viewCounter(){
+    ResponseEntity<Coupon> viewCounter() {
         Coupon coupon = couponService.decrease(1L);
         return ResponseEntity.ok(coupon);
     }
--- src/main/java/com/example/demo/service/CouponService.java
+++ src/main/java/com/example/demo/service/CouponService.java
@@ -1,18 +1,30 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Coupon;
+import com.example.demo.repository.CouponRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@CacheConfig(cacheNames = "default")
 public class CouponService {
     private final CouponDecreaseService couponDecreaseService;
-
+    private final CouponRepository couponRepository;
     private static final String COUPON_KEY_PREFIX = "COUPON_";
 
+    @CacheEvict(key = "'COUPON_' + #couponId")
     public Coupon decrease(Long couponId) {
         String couponName = COUPON_KEY_PREFIX + couponId;
-        return couponDecreaseService.couponDecrease(couponName);
+        return couponDecreaseService.couponDecrease(couponName); // TODO: couponDecreaseService 제거, 서비스 통합
+    }
+
+    @Cacheable(key = "'COUPON_' + #couponId") // TODO: 캐시 키 네이밍은 keyGenerator에서 자동으로 설정되도록 구현하기
+    public Coupon show(Long couponId) {
+        String couponName = COUPON_KEY_PREFIX + couponId;
+        return couponRepository.findByName(couponName);
     }
 }
